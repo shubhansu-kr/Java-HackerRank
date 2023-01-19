19fb2e016b3f0dbdc2d4dbe9a8f7d4637b1401c9
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-19 19:26:04 +0530

    Day4: Println
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 03_Day/00_Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int red = sc.nextInt(), green = sc.nextInt();
+        System.out.println(red + green);
+    }
+}
---
+++ 03_Day/01_String.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        System.out.println("The string is :"+ str);
+        System.out.println("The integer is :"+n);
+    }
+}
