488b11cfb4cc47e490bfbaa7dc0ff060a1205301
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-29 00:28:11 +0530

    Day09: Input & Conversion
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 09_Day/00_Input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int i = sc.nextInt();
+        if (i > 0) {
+            System.out.println((double)(i));
+        }
+        else {
+            System.out.println("Invalid Input");
+        }
+    }
+}
