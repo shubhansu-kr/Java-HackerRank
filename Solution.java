9de3ca86cf9392223bf5838a8abc5b9c467e6e51
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-13 20:21:17 +0530

    Practice
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Solution.java
+++ Solution.java
@@ -1,46 +1,14 @@
 import java.util.*;
 
 class Solution {
-
-    // enum Week{
-    //     Monday, Tuesday, Wednesday, Thursday, Friday, Saturdays;
-
-    //     String open, close;
-
-    //     Week(){
-    //         open = "8:30";:    //         close = "5.30";
-    //     }
-    // }
-
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
-        String input = sc.nextLine();
-        
-        switch (input) {
-            case "Monday":
-                System.out.println("8:30 5:30");
-                break;
-            case "Tuesday":
-                System.out.println("8:30 5:30");
-                break;
-            case "Wednesday":
-                System.out.println("8:30 5:30");
-                break;
-            case "Thursday":
-                System.out.println("8:30 5:30");
-                break;
-            case "Friday":
-                System.out.println("8:30 5:30");
-                break;
-            case "Saturdays":
-                System.out.println("9:30 4:30");
-                break;
-            default:
-                System.out.println("Library is closed");
-                break;
+        int []myList = {16, 15, 016};
+        for (int i = 0; i < myList.length; i++) {
+            System.out.print(myList[i] + " ");
+            
         }
-
         sc.close();
     }
 }
